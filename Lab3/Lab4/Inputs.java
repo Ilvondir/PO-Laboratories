3718daac80b6f1aab1c91e445832beabd5d3fd01
Ilvondir <101294147+Ilvondir@users.noreply.github.com>
2022-03-31 16:45:07 +0200

    Add files via upload

---
+++ Lab3/Lab4/Inputs.java
@@ -0,0 +1,16 @@
+package Lab4;
+
+import java.util.Scanner;
+
+public class Inputs {
+
+    public static int input(int x) {
+        Scanner input = new Scanner(System.in);
+        return input.nextInt();
+    }
+
+    public static double input(double x) {
+        Scanner input = new Scanner(System.in);
+        return input.nextDouble();
+    }
+}
---
+++ Lab3/Lab4/Main.java
@@ -0,0 +1,14 @@
+package Lab4;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\n\nEfekt dzialania zad. 1:");
+        Ex1.main();
+
+        System.out.println("\n\nEfekt dzialania zad. 2:");
+        Ex2.booleanTable();
+
+        System.out.println("\n\nEfekt dzialania zad. 3:");
+        Ex3.main();
+    }
+}
